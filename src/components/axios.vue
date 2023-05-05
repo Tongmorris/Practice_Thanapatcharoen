27f3ff934e7c514ab195afb647e757123c0fab96
Thanapatcharoen <132281623+Tongmorris@users.noreply.github.com>
2023-05-05 14:24:58 +0700

    Initial commit

---
+++ src/components/axios.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p v-for="manu in manufacturer" :key="manu.id">
+      {{ manu.id }}-{{ manu.Title }}
+    </p>
+    <input placeholder="id" v-model="id" />
+    <input placeholder="id" v-model="Title" />
+    <button @click="post()">Post</button>
+    <button @click="put()">Put</button>
+    <button @click="remove()">Delete</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      text: "test",
+      manufacturer: [{ id: 2, Title: "P" }],
+      id: "",
+      Title: "",
+    };
+  },
+  created() {
+    this.get();
+  },
+  methods: {
+    get() {
+      axios.get("https://localhost:7085/api/manufacturers").then((respone) => {
+        this.manufacturer = respone.data;
+      });
+    },
+  },
+};
+</script>
